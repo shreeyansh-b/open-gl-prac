eff0b77cb4106440ec70b43e578fcf032bc93650
shreeyansh-b <shebucr7@gmail.com>
2024-09-07 22:15:50 +0530

    added multi texture, mixing textures and colors

--- main.cpp
+++ main.cpp
@@ -57,15 +57,15 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    Shader ourShader("vert.hlsl", "frag.hlsl");
+    Shader ourShader("vert.glsl", "frag.glsl");
 
 
     
 
     // texture generation and init
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    unsigned int texture1,texture2 ;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
     // set the texture wrapping/filtering options (on the currently bound texture object)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -73,12 +73,27 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     // Texture loading
+    stbi_set_flip_vertically_on_load(true);
+
     int width, height, nrChannels;
     unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
 
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
 
 
     // VAO, VBO, EBO etc.
@@ -99,6 +114,14 @@ int main()
 
 
     int ourColorLocation = glGetUniformLocation(ourShader.ID, "ourColor");
+    int tex1Location = glGetUniformLocation(ourShader.ID, "texture1");
+    int tex2Location = glGetUniformLocation(ourShader.ID, "texture2");
+
+    ourShader.Activate();
+
+
+    glUniform1i(tex1Location, 0);
+    glUniform1i(tex2Location, 1);
 
 
 
@@ -117,7 +140,13 @@ int main()
 
         glUniform4f(ourColorLocation, redValue, greenValue, blueValue, 1.0f);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
+
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         ourShader.Activate();
         glBindVertexArray(VAO);
