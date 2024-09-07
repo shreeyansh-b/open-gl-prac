04cdc649ebfdf4e7837041f4083508c44a795380
shreeyansh-b <shebucr7@gmail.com>
2024-09-07 23:56:08 +0530

    prince vegeta texture

--- frag.glsl
+++ frag.glsl
@@ -4,12 +4,11 @@ out vec4 FragColor;
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
-uniform sampler2D texture2;
 
 uniform vec4 ourColor;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2) * ourColor;
+    FragColor = (texture(texture1, TexCoord));
 
 }
--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ int main()
     
 
     // texture generation and init
-    unsigned int texture1,texture2 ;
+    unsigned int texture1 ;
     glGenTextures(1, &texture1);
     glBindTexture(GL_TEXTURE_2D, texture1);
     // set the texture wrapping/filtering options (on the currently bound texture object)
@@ -76,24 +76,11 @@ int main()
     stbi_set_flip_vertically_on_load(true);
 
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    stbi_image_free(data);
-
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("vegeta.gif", &width, &height, &nrChannels, 0);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
+
 
 
     // VAO, VBO, EBO etc.
@@ -115,13 +102,11 @@ int main()
 
     int ourColorLocation = glGetUniformLocation(ourShader.ID, "ourColor");
     int tex1Location = glGetUniformLocation(ourShader.ID, "texture1");
-    int tex2Location = glGetUniformLocation(ourShader.ID, "texture2");
 
     ourShader.Activate();
 
 
     glUniform1i(tex1Location, 0);
-    glUniform1i(tex2Location, 1);
 
 
 
@@ -145,8 +130,7 @@ int main()
         // bind textures on corresponding texture units
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
+
 
         ourShader.Activate();
         glBindVertexArray(VAO);
