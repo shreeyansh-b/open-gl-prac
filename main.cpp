d3adadb1a219cf9133b81f96c3f23b0f89789ef9
shreeyansh-b <shebucr7@gmail.com>
2024-09-08 02:19:48 +0530

    rotating 3d vegetas

--- main.cpp
+++ main.cpp
@@ -214,7 +214,7 @@ int main()
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::rotate(model, glm::radians(timeValue * i * i), glm::vec3(1.0f, 0.3f, 0.5f));
             glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
